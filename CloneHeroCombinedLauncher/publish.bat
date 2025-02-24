69722df97c0ee06c315d17ec616f88d2c1c26161
Jakub Grula <ramsters110@gmail.com>
2025-02-24 21:40:39 +0100

    Fixed publishing script

--- CloneHeroCombinedLauncher/publish.bat
+++ CloneHeroCombinedLauncher/publish.bat
@@ -1,1 +1,13 @@
-dotnet publish -c Release -r win-x64 --self-contained --single-file -p:AssemblyName="CloneHeroCombinedLauncher-64bit.exe
+@echo off
+setlocal
+
+rem x64
+dotnet publish -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:AssemblyName=CloneHeroCombinedLauncher-64bit
+
+rem x86
+dotnet publish -c Release -r win-x86 --self-contained -p:PublishSingleFile=true -p:AssemblyName=CloneHeroCombinedLauncher-32bit
+
+echo Done
+endlocal
+pause
+
